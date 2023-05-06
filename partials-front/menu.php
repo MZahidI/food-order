c350c56e35fc38b0c18c3e086fd62f87b6c4b486
MZahidI <50088846+MZahidI@users.noreply.github.com>
2023-05-07 03:00:34 +0600

    Add files via upload

---
+++ partials-front/menu.php
@@ -0,0 +1,51 @@
+<?php include('config/constants.php'); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wowFood - The Food Heaven</title>
+
+    <!-- Adding Our Food Icon -->
+    <link rel="icon" href="images/icon.png">
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="<?php echo SITEURL; ?>" title="wowFood">
+                    <img src="<?php echo SITEURL; ?>images/logo.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>">Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>categories.php">Categories</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>foods.php">Foods</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+
+    <!-- <marquee style="color:#ff4757;">Welcome to wowFood - The Food Heaven.</marquee> -->
+    <!-- Navbar Section Ends Here -->
